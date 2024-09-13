df2c965294c33ac55107f3cbbe6bac97e74580dc
cyril-pierro <fiopapa32@gmail.com>
2024-09-13 14:40:54 +0000

    multiple value

--- parser/report.py
+++ parser/report.py
@@ -12,7 +12,7 @@ class ReportParser:
                 "number_of_engineers": value[1]
             } for value in data
         ]
-    
+
     @staticmethod
     def convert_department_adjustment_orders_data(data: list[dict[Any, str]]):
         if not data:
@@ -25,7 +25,7 @@ class ReportParser:
             }
             for value in data
         ]
-    
+
     @staticmethod
     def convert_number_and_quantity_orders_data(data: list[dict[Any, str]]):
         if not data:
@@ -35,7 +35,7 @@ class ReportParser:
                 "department": value[0],
                 "number_of_orders":  value[1] or 0,
                 "quantity_of_orders": value[2] or 0,
-                "total_cost": value[3] or 0,
+                "total_cost": (value[3] * value[2]) or 0,
             }
             for value in data
         ]
